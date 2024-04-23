66d614d60e957ae40bdc833ecffc27f5b4476c60
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-04-23 21:54:22 +0500

    Sort DONE

--- HomeWork/main.cpp
+++ HomeWork/main.cpp
@@ -331,17 +331,18 @@ void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS)
 	{
 		for (int j = 0; j < COLS; j++) 
 		{
-			for (int k = 0; k < COLS - j - 1; k++) 
+			for (int k = i; k < COLS - j - 1; k++) 
 			{
-				if (arr[i][k] > arr[i][k + 1])
+				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
+				if(arr[k][l]<arr[i][j])
 				{
-					int buffer = arr[i][k];
-					arr[i][k] = arr[i][k + 1];
-					arr[i][k + 1] = buffer;
-				} 
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+				}
 			}
 		}
-	} cout << endl;
+	}
 }
 void Sort(double arr[][COLS], const int ROWS, const int COLS)
 {
